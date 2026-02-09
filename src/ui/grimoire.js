54265c23471f5731ec87e769a1fdbc3760873eac
Judas <sinnerconsort@gmail.com>
2026-02-09 13:34:20 -0500

    Adjust top position in grimoire.js style

--- src/ui/grimoire.js
+++ src/ui/grimoire.js
@@ -327,7 +327,7 @@ function createContent(book, scale, offsetY) {
         position: absolute !important;
         left: 37% !important;
         right: 20% !important;
-        top: 30% !important;
+        top: 25% !important;
         bottom: 23% !important;
         padding: 2% !important;
         overflow-y: auto !important;
